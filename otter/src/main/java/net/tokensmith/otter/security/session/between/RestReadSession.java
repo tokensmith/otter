c09669c61684a69e8f0a6a86f8b70735035e0f53
= <tom.s.mackenzie@gmail.com>
2020-03-17 16:26:48 -0500

    176: RestReadSession a between to read a session for rest resources

---
+++ otter/src/main/java/net/tokensmith/otter/security/session/between/RestReadSession.java
@@ -0,0 +1,90 @@
+package net.tokensmith.otter.security.session.between;
+
+import net.tokensmith.otter.controller.entity.Cookie;
+import net.tokensmith.otter.controller.entity.StatusCode;
+import net.tokensmith.otter.dispatch.entity.RestBtwnRequest;
+import net.tokensmith.otter.dispatch.entity.RestBtwnResponse;
+import net.tokensmith.otter.router.entity.Method;
+import net.tokensmith.otter.router.entity.between.RestBetween;
+import net.tokensmith.otter.router.exception.HaltException;
+import net.tokensmith.otter.security.session.between.exception.InvalidSessionException;
+import net.tokensmith.otter.security.session.between.exception.SessionDecryptException;
+import net.tokensmith.otter.security.session.between.util.Decrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+
+
+/**
+ * A RestBetween that will read and decrypt a session that can be used to assign a user to the request.
+ * This is useful for requests to an API from a browser.
+ *
+ * @param <S> Session to decrypt
+ * @param <U> The User to pass along to the RestResource
+ */
+public class RestReadSession<S, U> implements RestBetween<U> {
+    protected static Logger LOGGER = LoggerFactory.getLogger(RestReadSession.class);
+    public static final String COOKIE_NOT_PRESENT = "session cookie not present.";
+    public static final String INVALID_SESSION_COOKIE = "Invalid value for the session cookie";
+
+    private String sessionCookieName;
+    private Boolean required;
+    private StatusCode failStatusCode;
+    private Decrypt<S> decrypt;
+
+    public RestReadSession(String sessionCookieName, Boolean required, StatusCode failStatusCode, Decrypt<S> decrypt) {
+        this.sessionCookieName = sessionCookieName;
+        this.required = required;
+        this.failStatusCode = failStatusCode;
+        this.decrypt = decrypt;
+    }
+
+    @Override
+    public void process(Method method, RestBtwnRequest<U> request, RestBtwnResponse response) throws HaltException {
+        Optional<S> session = readSession(request, response);
+        process(method, request, response, session);
+    }
+
+    protected void process(Method method, RestBtwnRequest<U> request, RestBtwnResponse response, Optional<S> session) throws HaltException {
+        // override this method with what is needed with the session.
+
+        // this is only here so it can be tested.
+        response.setStatusCode(StatusCode.ACCEPTED);
+    }
+
+    protected Optional<S> readSession(RestBtwnRequest<U> request, RestBtwnResponse response) throws HaltException {
+        Optional<S> session = Optional.empty();
+        Cookie sessionCookie = request.getCookies().get(sessionCookieName);
+
+        if (sessionCookie == null && required) {
+            HaltException halt = new HaltException(COOKIE_NOT_PRESENT);
+            onHalt(halt, response);
+            throw halt;
+        } else if (sessionCookie == null && !required) {
+            // ok to proceed to resource. The session is not required.
+            return session;
+        }
+
+        try {
+            session = Optional.of(decrypt.decrypt(sessionCookie.getValue()));
+        } catch (InvalidSessionException e) {
+            LOGGER.error(e.getMessage(), e);
+            HaltException halt = new HaltException(INVALID_SESSION_COOKIE, e);
+            onHalt(halt, response);
+            throw halt;
+        } catch (SessionDecryptException e) {
+            LOGGER.error(e.getMessage(), e);
+            HaltException halt = new HaltException(INVALID_SESSION_COOKIE, e);
+            onHalt(halt, response);
+            throw halt;
+        }
+
+        return session;
+    }
+
+    protected void onHalt(HaltException e, RestBtwnResponse response) {
+        response.setStatusCode(failStatusCode);
+        response.getCookies().remove(sessionCookieName);
+    }
+}
